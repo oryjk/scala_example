302c217b72dfcf83187f533923f88ff7fafa1a02
Carl Wang <oryjk@qq.com>
2020-02-25 22:52:11 +0800

    add zpx

--- src/main/scala/com/carl/ConstructTest.scala
+++ src/main/scala/com/carl/ConstructTest.scala
@@ -2,11 +2,6 @@ package com.carl
 
 object ConstructTest extends App {
 
-  new A
 
 }
 
-class A {
-  val name: String = "hello"
-  println(name)
-}
---
+++ src/main/scala/com/carl/Zpx.scala
@@ -0,0 +1,9 @@
+package com.carl
+
+/**
+ * Created by WangRui on 2020/2/25.
+ */
+object Zpx extends App {
+  println("zpx")
+
+}
